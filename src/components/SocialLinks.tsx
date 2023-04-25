5db7fd264a6fc7dcba09d97ec96575e9e9440e0a
Matthew Wigglesworth <m.p.wigglesworth@gmail.com>
2023-04-25 13:51:27 -0700

    social media hover

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -9,30 +9,34 @@ const socialLinks = [
   {
     href: 'https://github.com/matt-wigg/',
     icon: FaGithub,
+    hover: 'hover:text-white dark:hover:text-white',
   },
   {
     href: 'https://www.linkedin.com/in/matt-wigg/',
     icon: FaLinkedin,
-  },
-  {
-    href: 'https://www.instagram.com/__matt_wigg/',
-    icon: FaInstagram,
+    hover: 'hover:text-blue-500 dark:hover:text-blue-400',
   },
   {
     href: 'https://twitter.com/MrSoftwareGuy',
     icon: FaTwitter,
+    hover: 'hover:text-blue-500 dark:hover:text-blue-400',
+  },
+  {
+    href: 'https://www.instagram.com/__matt_wigg/',
+    icon: FaInstagram,
+    hover: 'hover:text-pink-500 dark:hover:text-pink-400',
   },
 ];
 
 const SocialLinks = () => {
   return (
     <div className='flex items-center justify-start space-x-4'>
-      {socialLinks.map(({ href, icon: Icon }) => (
+      {socialLinks.map(({ href, icon: Icon, hover }) => (
         <Link key={href} href={href} legacyBehavior>
           <a
             target='_blank'
             rel='noopener noreferrer'
-            className='group bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-200 dark:border-gray-700 flex items-center justify-start focus:outline-none focus:ring-2 focus:ring-yellow-400 dark:focus:ring-yellow-400'
+            className={`group bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-200 dark:border-gray-700 flex items-center justify-start focus:outline-none focus:ring-2 focus:ring-yellow-400 ${hover} dark:focus:ring-yellow-400`}
           >
             <Icon className='h-4 w-4' />
           </a>
