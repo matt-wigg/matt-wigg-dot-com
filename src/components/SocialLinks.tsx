cc201c67a6398c8238d9b3c437b01bb55208b3eb
Matthew Wigglesworth <m.p.wigglesworth@gmail.com>
2023-04-25 13:18:48 -0700

    socialLinks to next/link

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,41 +1,41 @@
-import { ArrowDownCircleIcon } from '@heroicons/react/24/outline';
+'use client';
+
+// components/SocialLinks.tsx
+
+import React from 'react';
 import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
+import Button from './Button';
+import Link from 'next/link';
+
+const socialLinks = [
+  {
+    href: 'https://github.com/matt-wigg/',
+    icon: FaGithub,
+  },
+  {
+    href: 'https://www.linkedin.com/in/matt-wigg/',
+    icon: FaLinkedin,
+  },
+  {
+    href: 'https://www.instagram.com/__matt_wigg/',
+    icon: FaInstagram,
+  },
+  {
+    href: 'https://twitter.com/MrSoftwareGuy',
+    icon: FaTwitter,
+  },
+];
 
 const SocialLinks = () => {
   return (
     <div className='flex items-center justify-start space-x-4'>
-      <a
-        href='https://github.com/matt-wigg/'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-yellow-400 transition-colors duration-200'
-      >
-        <FaGithub className='h-4 w-4' />
-      </a>
-      <a
-        href='https://www.linkedin.com/in/matt-wigg/'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-yellow-400 transition-colors duration-200'
-      >
-        <FaLinkedin className='h-4 w-4' />
-      </a>
-      <a
-        href='https://www.instagram.com/__matt_wigg/'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-yellow-400 transition-colors duration-200'
-      >
-        <FaInstagram className='h-4 w-4' />
-      </a>
-      <a
-        href='https://twitter.com/MrSoftwareGuy'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-yellow-400 transition-colors duration-200'
-      >
-        <FaTwitter className='h-4 w-4' />
-      </a>
+      {socialLinks.map(({ href, icon: Icon }) => (
+        <Link key={href} href={href} target='_blank' passHref>
+          <Button className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-yellow-400 transition-colors duration-200'>
+            <Icon className='h-4 w-4' />
+          </Button>
+        </Link>
+      ))}
     </div>
   );
 };
