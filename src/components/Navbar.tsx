17e1616180ee18ae35a3b52f6eaea9e57aa7a2ab
Matthew Wigglesworth <m.p.wigglesworth@gmail.com>
2023-04-25 20:53:22 -0700

    mobile light mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,14 +51,14 @@ const Navbar = () => {
 
   return (
     <div className='flex flex-col md:flex-row md:space-x-4 md:space-y-0'>
-      <div className='md:flex bg-white dark:bg-zinc-950 dark:text-gray-100 w-64 min-w-64 h-full flex-col justify-between hidden top-0 left-0 border-r border-gray-800 fixed'>
+      <div className='md:flex  dark:bg-zinc-950 dark:text-gray-100 w-64 min-w-64 h-full flex-col justify-between hidden top-0 left-0 border-r border-gray-800 fixed'>
         <nav className='px-6 py-4 space-y-4'>
           <h1 className='font-mono text-2xl py-2 font-medium text-gray-900 dark:text-gray-100'>
             matt-wigg-dot-com
           </h1>
           <Link href='/' legacyBehavior>
             <Button
-              className={`bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
+              className={` dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
                 pathname === '/' ? 'text-yellow-400' : 'dark:text-gray-100'
               }`}
             >
@@ -76,7 +76,7 @@ const Navbar = () => {
           </Link>
           <Link href='/projects' legacyBehavior>
             <Button
-              className={`bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
+              className={` dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
                 pathname === '/projects'
                   ? 'text-yellow-400'
                   : 'dark:text-gray-100'
@@ -96,7 +96,7 @@ const Navbar = () => {
           </Link>
           <Link href='/blog' legacyBehavior>
             <Button
-              className={`bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
+              className={` dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
                 pathname === '/blog' ? 'text-yellow-400' : 'dark:text-gray-100'
               }`}
             >
@@ -114,7 +114,7 @@ const Navbar = () => {
           </Link>
           <Link href='/contact' legacyBehavior>
             <Button
-              className={`bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
+              className={` dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full dark:hover:text-yellow-400 ${
                 pathname === '/contact'
                   ? 'text-yellow-400'
                   : 'dark:text-gray-100'
@@ -138,11 +138,16 @@ const Navbar = () => {
 
       <div className='w-full'>
         {/* Mobile Navbar */}
-        <div className='md:hidden flex items-center justify-between bg-zinc-950 p-4 border-b border-gray-700 dark:border-gray-700'>
-          <p className='text-white font-mono'>matt-wigg-dot-com</p>
-          <Button className='text-yellow-400' onClick={toggleMenu}>
+        <div className='md:hidden flex items-center justify-between bg-white dark:bg-zinc-950 p-4 border-b-2 border-gray-800 dark:border-gray-700'>
+          <p className='text-gray-900 dark:text-white font-mono'>
+            matt-wigg-dot-com
+          </p>
+          <Button
+            className='text-yellow-400 bg-transparent'
+            onClick={toggleMenu}
+          >
             <svg
-              className='w-6 h-6'
+              className='w-6 h-6 text-gray-900 dark:text-white'
               fill='none'
               stroke='currentColor'
               viewBox='0 0 24 24'
@@ -158,11 +163,15 @@ const Navbar = () => {
           </Button>
         </div>
         {isOpen && (
-          <nav className='border-b border-gray-700 dark:border-gray-700 md:hidden bg-zinc-950 p-4 space-y-2 text-gray-100 absolute top-16 w-full z-10'>
+          <nav className='border-b-2 border-gray-800 dark:border-gray-700 md:hidden bg-white dark:bg-zinc-950 p-4 space-y-2 absolute top-16 w-full z-10'>
             <div className='flex flex-col w-full space-y-2'>
               <Link href='/' legacyBehavior>
                 <Button
-                  className='bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full'
+                  className={`bg-gray-100 dark:bg-zinc-950 hover:bg-gray-200 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full ${
+                    pathname === '/'
+                      ? 'text-yellow-400'
+                      : 'text-gray-900 dark:text-gray-100'
+                  }`}
                   onClick={() => toggleMenu()}
                 >
                   <UserCircleIcon className='h-6 w-6 text-yellow-400' />
@@ -171,7 +180,11 @@ const Navbar = () => {
               </Link>
               <Link href='/projects' legacyBehavior>
                 <Button
-                  className='bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full'
+                  className={`bg-gray-100 dark:bg-zinc-950 hover:bg-gray-200 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full ${
+                    pathname === '/projects'
+                      ? 'text-yellow-400'
+                      : 'text-gray-900 dark:text-gray-100'
+                  }`}
                   onClick={() => toggleMenu()}
                 >
                   <CommandLineIcon className='h-6 w-6 text-yellow-400' />
@@ -180,7 +193,11 @@ const Navbar = () => {
               </Link>
               <Link href='/blog' legacyBehavior>
                 <Button
-                  className='bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full'
+                  className={`bg-gray-100 dark:bg-zinc-950 hover:bg-gray-200 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full ${
+                    pathname === '/blog'
+                      ? 'text-yellow-400'
+                      : 'text-gray-900 dark:text-gray-100'
+                  }`}
                   onClick={() => toggleMenu()}
                 >
                   <BookOpenIcon className='h-6 w-6 text-yellow-400' />
@@ -189,7 +206,11 @@ const Navbar = () => {
               </Link>
               <Link href='/contact' legacyBehavior>
                 <Button
-                  className='bg-white dark:bg-zinc-950 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full'
+                  className={`bg-gray-100 dark:bg-zinc-950 hover:bg-gray-200 dark:hover:bg-zinc-900 rounded-md px-4 py-2 border border-gray-700 dark:border-gray-700 flex items-center justify-start w-full ${
+                    pathname === '/contact'
+                      ? 'text-yellow-400'
+                      : 'text-gray-900 dark:text-gray-100'
+                  }`}
                   onClick={() => toggleMenu()}
                 >
                   <PencilSquareIcon className='h-6 w-6 text-yellow-400' />
@@ -197,6 +218,7 @@ const Navbar = () => {
                 </Button>
               </Link>
             </div>
+            <ToggleTheme />
           </nav>
         )}
         {/* Content */}
