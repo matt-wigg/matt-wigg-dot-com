fb6d12034474f7fac64a4adc8b0efc2f72b52647
Matthew Wigglesworth <m.p.wigglesworth@gmail.com>
2023-05-27 15:27:49 -0700

    fix show=false contact page & padding

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,15 +1,14 @@
-import { Inter } from 'next/font/google';
 import ContactForm from '@/components/ContactForm';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Contact() {
   return (
-    <main className='flex md:flex-row flex-col pt-4'>
-      <div className='md:flex md:flex-col'>
-        <ContactForm show={true} />
+    <main className='flex md:flex-row flex-col pt-8'>
+      <div className='md:w-3/5 md:min-w-[500px] md:flex md:flex-col'>
+        <div className='pb-4'>
+          <ContactForm show={true} />
+        </div>
       </div>
-      <div className='md:w-3/5 md:flex md:flex-col'>
+      <div className='md:w-2/5 md:flex md:flex-col'>
         <div className='pb-4'></div>
       </div>
     </main>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,17 +2,15 @@ import ChatForm from '@/components/ChatForm';
 
 export default function Home() {
   return (
-    <>
-      <main className='flex md:flex-row flex-col pt-8'>
-        <div className='md:w-3/5 md:flex md:flex-col'>
-          <div className='pb-4'>
-            <ChatForm show={true} />
-          </div>
+    <main className='flex md:flex-row flex-col pt-8'>
+      <div className='md:w-3/5 md:min-w-[500px] md:flex md:flex-col'>
+        <div className='pb-4'>
+          <ChatForm show={true} />
         </div>
-        <div className='md:w-2/5 md:flex md:flex-col'>
-          <div className='pb-4'></div>
-        </div>
-      </main>
-    </>
+      </div>
+      <div className='md:w-2/5 md:flex md:flex-col'>
+        <div className='pb-4'></div>
+      </div>
+    </main>
   );
 }
