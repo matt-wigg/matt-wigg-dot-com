5d323c7c78acd30ec88840c4b948e275736e081b
Matt-Wigg <m.p.wigglesworth@gmail.com>
2023-09-05 05:42:13 -0500

    align timeline to linkedin

--- src/components/ProfesionalTimeline.tsx
+++ src/components/ProfesionalTimeline.tsx
@@ -1,60 +1,60 @@
-import ContentCard from '@/components/ContentCard/ContentCard';
+import ContentCard from "@/components/ContentCard/ContentCard";
 
 const ProfesionalTimeline = () => {
   const timelineData = [
     {
-      date: '2021 - Present',
-      title: 'Software Engineer II',
-      company: 'HTLF',
-      country: '🇺🇸',
+      date: "2021 - Present",
+      title: "Full Stack Engineer | Digital Tech. Supervisor, AVP",
+      company: "HTLF",
+      country: "🇺🇸",
     },
     {
-      date: '2020 - 2021',
-      title: 'Software Engineer',
-      company: 'Freelance / Contract',
-      country: '🇺🇸',
+      date: "2020 - 2021",
+      title: "Full Stack Engineer",
+      company: "Freelance / Contract",
+      country: "🇺🇸",
     },
     {
-      date: '2020 - 2020',
-      title: 'Advanced Software Engineering',
-      company: 'Hack Reactor',
-      country: '🇺🇸',
+      date: "2020 - 2020",
+      title: "Advanced Software Engineering",
+      company: "Hack Reactor",
+      country: "🇺🇸",
     },
     {
-      date: '2013 - 2019',
-      title: 'Director of Ecommerce',
-      company: 'Firespares / Stove Supermarket',
-      country: '🇬🇧',
+      date: "2013 - 2019",
+      title: "Director of Ecommerce",
+      company: "Firespares / Stove Supermarket",
+      country: "🇬🇧",
     },
     {
-      date: '2008 - 2010',
-      title: 'FdA in Graphic Design',
-      company: 'University of the Arts London',
-      country: '🇬🇧',
+      date: "2008 - 2010",
+      title: "FdA in Graphic Design",
+      company: "University of the Arts London",
+      country: "🇬🇧",
     },
   ];
 
   return (
     <ContentCard
       show={false}
-      title='Professional Timeline'
+      title="Professional Timeline"
       content={
-        <ul className='space-y-4'>
+        <ul className="space-y-4">
           {timelineData.map((item, index) => (
             <li key={index}>
-              <div className='flex justify-between items-start'>
-                <div className='flex items-center space-x-4'>
+              <div className="flex justify-between items-start">
+                <div className="flex items-center space-x-4">
                   <div>
-                    <p className='text-gray-900 dark:text-gray-300 font-semibold'>
+                    <p className="text-gray-900 dark:text-gray-300 font-semibold">
                       {item.title}
                     </p>
-                    <p className='text-gray-500 dark:text-gray-400 font-extralight'>
+                    <p className="text-gray-500 dark:text-gray-400 font-extralight">
                       {item.company}
-                      <span className='ml-2 text-lg'>{item.country}</span>
+                      <span className="ml-2 text-lg">{item.country}</span>
                     </p>
                   </div>
                 </div>
-                <span className='text-gray-600 dark:text-gray-400 font-extralight'>
+                <span className="text-gray-600 dark:text-gray-400 font-extralight">
                   {item.date}
                 </span>
               </div>
