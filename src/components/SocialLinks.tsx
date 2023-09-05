08217d57510a3e38f7c0c46e02a9bf95b50c98ac
Matt-Wigg <m.p.wigglesworth@gmail.com>
2023-09-05 05:37:26 -0500

    remove instagram

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,37 +1,32 @@
-import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
-import Link from 'next/link';
-import Button from '@/components/Button';
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
+import Button from "@/components/Button";
 
 const socialLinks = [
   {
-    href: 'https://github.com/matt-wigg/',
+    href: "https://github.com/matt-wigg/",
     icon: FaGithub,
-    hover: 'hover:text-black dark:hover:text-white',
+    hover: "hover:text-black dark:hover:text-white",
   },
   {
-    href: 'https://www.linkedin.com/in/matt-wigg/',
+    href: "https://www.linkedin.com/in/matt-wigg/",
     icon: FaLinkedin,
-    hover: 'hover:text-blue-500 dark:hover:text-blue-400',
+    hover: "hover:text-blue-500 dark:hover:text-blue-400",
   },
   {
-    href: 'https://www.instagram.com/__matt_wigg/',
-    icon: FaInstagram,
-    hover: 'hover:text-pink-500 dark:hover:text-pink-400',
-  },
-  {
-    href: 'https://twitter.com/MrSoftwareGuy',
+    href: "https://twitter.com/MrSoftwareGuy",
     icon: FaTwitter,
-    hover: 'hover:text-blue-500 dark:hover:text-blue-400',
+    hover: "hover:text-blue-500 dark:hover:text-blue-400",
   },
 ];
 
 const SocialLinks = () => {
   return (
-    <div className='flex items-center justify-start space-x-4'>
+    <div className="flex items-center justify-start space-x-4">
       {socialLinks.map(({ href, icon: Icon, hover }) => (
-        <Link key={href} href={href} target='_blank'>
+        <Link key={href} href={href} target="_blank">
           <Button className={`group ${hover}`}>
-            <Icon className='h-4 w-4' />
+            <Icon className="h-4 w-4" />
           </Button>
         </Link>
       ))}
