b032759e2dedc2983c4f2034018852a76eb0c9bd
Matt-Wigg <m.p.wigglesworth@gmail.com>
2023-09-05 12:23:52 -0500

    linkedin first

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -4,14 +4,14 @@ import Button from "@/components/Button";
 
 const socialLinks = [
   {
-    href: "https://github.com/matt-wigg/",
-    icon: FaGithub,
-    hover: "hover:text-black dark:hover:text-white",
-  },
-  {
     href: "https://www.linkedin.com/in/matt-wigg/",
     icon: FaLinkedin,
     hover: "hover:text-blue-500 dark:hover:text-blue-400",
+  },
+  {
+    href: "https://github.com/matt-wigg/",
+    icon: FaGithub,
+    hover: "hover:text-black dark:hover:text-white",
   },
 ];
 
