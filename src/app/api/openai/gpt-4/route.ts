20d5e8fced365bf8ca58a66430257f04b43992aa
Matt-Wigg <m.p.wigglesworth@gmail.com>
2023-09-26 10:53:22 -0500

    update gpt env

--- src/app/api/openai/gpt-4/route.ts
+++ src/app/api/openai/gpt-4/route.ts
@@ -5,7 +5,7 @@ import { ErrorRes } from "@/types/ErrorTypes";
 
 // Initialize OpenAI
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY!,
+  apiKey: process.env.OPEN_AI_KEY!,
 });
 
 // Constants
