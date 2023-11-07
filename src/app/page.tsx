04c5f3fc60c0865d1f5f9f1098fb69fcf75bd12b
Matt-Wigg <m.p.wigglesworth@gmail.com>
2023-11-06 21:26:08 -0600

    stack order min height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <HomeTabs />
           </div>
         </div>
-        <div className="xl:col-span-2 xl:row-start-auto xl:row-end-auto xl:min-h-[10%]">
+        <div className="xl:col-span-2 xl:row-start-auto xl:row-end-auto xl:min-h-[120%]">
           <div className="pb-4">
             <Profiles />
           </div>
